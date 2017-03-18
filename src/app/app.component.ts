e829e40cb787d1ce010d68ec83958a48dbe4db6b
Sharad Purohit <sharad.purohit@ge.com>
2017-03-18 12:41:14 +0530

    Initial setup commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `<h1>This is, {{name}}`,
+})
+export class AppComponent { name = 'Question Paper Builder.'; }
